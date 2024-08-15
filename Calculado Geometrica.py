31498d1ecb90a77f13160eb29b9326bab2c3bc5f
Andrey-Figueroa <andreyf1887@gmail.com>
2024-08-15 12:05:01 -0600

    Add files via upload

--- Calculado Geometrica.py
+++ Calculado Geometrica.py
@@ -32,4 +32,36 @@ def triangulo():
         print("La opcion no existe")
     print(f"El {operacion} del rectangulo es de: {resultado}")
 #triangulo()
- 
+def distanciaPuntos():
+    import math
+    print("Calcular distancia entre puntos")
+    x1 = float(input("Indique cual es tu x1: "))
+    y1 = float(input("Indique cual es tu y1: "))
+    x2 = float(input("Indique cual es tu x2: "))
+    y2 = float(input("Indique cual es tu y2: "))
+    distancia = math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
+    print(f"La distancia entre los puntos es de {distancia:.2f}.")
+#distanciaPuntos()
+def circulo():
+    import math
+    print("Opciones:\n1-Area de la esfera\n2-Volumen de la esfera")
+    opcionCirculo = int(input("Cual opcion deseas: "))
+    radioDiametro = input("Con que deseas realizar tu ejercicio (diametro o radio): ")
+
+    if radioDiametro.lower() == "diametro":
+        diametro = float(input("Cuanto mide el diametro: "))
+        radio = diametro / 2
+    elif radioDiametro.lower() == "radio":
+        radio = float(input("Cuanto mide el radio: "))
+    else:
+        print("La opcion no existe")
+    if opcionCirculo == 1:
+        operacion = "Area"
+        resultado = math.pi * 4 * radio**2
+    elif opcionCirculo == 2:
+        operacion = "Volumen"
+        resultado = 1/3 * math.pi * radio**3
+    else:
+        print("La opcion no existe")
+    print(f"El {operacion} de la esfera es de: {resultado:.2f}.")
+circulo()
